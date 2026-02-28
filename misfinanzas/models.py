9ac116b22118a6e33bd90762cbe22e29e34abca1
Alessandro <alessandrodini97@gmail.com>
2026-02-28 11:04:46 -0300

    Agrega las bases para la función de presupuesto

--- misfinanzas/models.py
+++ misfinanzas/models.py
@@ -6,10 +6,14 @@ from django.contrib.auth.models import User
 
 class Gasto(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    precio = models.IntegerField()
+    precio = models.FloatField()
     categoria = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=255)
     fecha = models.DateField()
     
     def __str__(self):
         return f"{self.categoria} - ${self.precio}"
+
+class Presupuesto(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    limite_mensual = models.FloatField(default=0)
